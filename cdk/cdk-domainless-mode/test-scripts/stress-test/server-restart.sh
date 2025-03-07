4cfd554f2db62cced50489be0607af8a2f38cbb1
muskanl <muskanl@amazon.com>
2025-03-07 15:23:08 -0800

    update server restart script

--- cdk/cdk-domainless-mode/test-scripts/stress-test/server-restart.sh
+++ cdk/cdk-domainless-mode/test-scripts/stress-test/server-restart.sh
@@ -3,6 +3,6 @@ do
   systemctl stop credentials-fetcher.service
   sleep 2
   systemctl start credentials-fetcher.service
-  echo "Restarted credentials fetcher after 30 seconds..."
   sleep 30
+  echo "Restarted credentials fetcher after 30 seconds..."
 done
